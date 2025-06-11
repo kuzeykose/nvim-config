f7dfdb0084f2dda0a7c7b1d83ee51b089d59c4ec
Kuzey Kose <kosekuzey97@gmail.com>
2025-06-10 22:34:08 -0400

    add python autoformat

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,13 +3,21 @@ lsp.preset("recommended")
 
 -- Do NOT use `lsp.ensure_installed` in v2
 -- Instead use mason-lspconfig directly:
-require("mason").setup({})
+require("mason").setup({
+    ensure_installed = {
+        "black"
+    }
+})
 require("mason-lspconfig").setup({
     ensure_installed = {
-        'tsserver',
+        'pyright',
         'eslint',
-        'gopls',
-        'jdtls',
+        'html',
+        'cssls',
+        'jsonls',
+        'emmet_ls',
+        'tailwindcss',
+        'lua_ls',
     },
     handlers = {
         lsp.default_setup,
@@ -29,6 +37,16 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("LspFormat." .. bufnr, { clear = true }),
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr, async = false })
+            end,
+        })
+    end
 end)
 
 lsp.setup()
--- lua/kuzey/remap.lua
+++ lua/kuzey/remap.lua
@@ -1,6 +1,36 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", function()
+    local filetype = vim.bo.filetype
+    local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+    local supports_format = false
+
+    for _, client in ipairs(clients) do
+        if client.supports_method("textDocument/formatting") then
+            supports_format = true
+            break
+        end
+    end
+
+    if supports_format then
+        vim.lsp.buf.format()
+    elseif filetype == "python" then
+        local filepath = vim.fn.expand("%:p")
+        vim.fn.jobstart({ "black", filepath }, {
+            on_exit = function(_, code, _)
+                if code == 0 then
+                    vim.cmd("edit!")
+                    vim.notify("Formatted with Black (fallback)", vim.log.levels.INFO)
+                else
+                    vim.notify("Black formatting failed", vim.log.levels.ERROR)
+                end
+            end,
+        })
+    else
+        vim.notify("No formatter available", vim.log.levels.WARN)
+    end
+end, { desc = "Format file (LSP or fallback)" })
+
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
